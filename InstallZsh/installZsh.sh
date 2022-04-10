b9d599685f425b2ee8914129e829b258331ba8e8
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2022-04-10 13:20:34 -0400

    Add script outline for zsh and resources

---
+++ InstallZsh/installZsh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Install ZSH and set it as the default
+
+echo "TODO"
---
+++ resources/installResources.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Install common resources that I like such as fonts and mouse cursors.
+
+echo "TODO"
